e517a20a8d994407de7553dd58ad98f310dd02ab
Harry Coleman <harrymcoleman@gmail.com>
2024-07-12 10:41:42 -0700

    add tui boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,28 @@ use log::debug;
 use log::info;
 use std::fs; // import the debug macro from the log crate
 
-mod words;
+use std::io;
+
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Alignment, Rect},
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    Frame,
+};
 
 // -----------------------------------------------------------------------------
 
-fn main() {
-    colog::basic_builder()
-        .filter(None, log::LevelFilter::Debug)
-        .init();
-    info!("logging initialized");
+mod tui;
+mod words;
 
-    let wl = words::WordList::from_file("./lists/words_alpha.txt");
-
-    wl.log_stats();
-
-    let filtered_words = wl.find_scrabble_simple("least");
-
-    // debug output filtered_words
-    println!("found {} words", filtered_words.clone().len(),);
-
-    for word in filtered_words {
-        println!("{}", word);
-    }
-}
+// =============================================================================
 
 // predicates things i need:
 // - P(word, letter) -> word contains the given letter
@@ -36,3 +35,109 @@ fn main() {
 // - has_letter(letter)
 // - has_letters(letters)
 // - made_of(letters) = word contains all the given letters and only those letters (with and without repeats) in other words, the word is a permutation/anagram of the given letters. this is good for scrabble and anagrams and anything where you have a specific bank of letters to use
+
+
+
+// =============================================================================
+
+fn main() -> io::Result<()> {
+    colog::basic_builder()
+        .filter(None, log::LevelFilter::Debug)
+        .init();
+    info!("logging initialized");
+
+    let mut terminal = tui::init()?;
+    let app_result = App::default().run(&mut terminal);
+    tui::restore()?;
+    app_result
+}
+
+// -----------------------------------------------------------------------------
+
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
+
+impl App {
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut tui::Tui) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.render_frame(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size())
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            KeyCode::Left => self.decrement_counter(),
+            KeyCode::Right => self.increment_counter(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Word Finder ".bold());
+
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
--- src/words.rs
+++ src/words.rs
@@ -7,8 +7,9 @@ impl WordList {
         let file = std::fs::read_to_string(file_path).expect("failed to read file");
 
         let words = file
-            .split_whitespace()
-            .map(|w| w.to_string())
+            .split_whitespace()                                 // assume one word per line
+            .map(|w| w.to_string().to_lowercase())              // convert to lowercase
+            .filter(|w| w.chars().all(|c| c.is_alphabetic()))   // only alphabetic
             .collect::<Vec<String>>();
 
         let mut info_string = format!("found {} words in file {}", words.len(), file_path);
@@ -41,9 +42,12 @@ impl WordList {
 
     // -------------------------------------------------------------------------
 
+    pub fn find(&self, predicate: impl Fn(&String) -> bool) -> Vec<&String> {
+        self.words.iter().filter(|word| predicate(word)).collect()
+    }
 
     /// Find words that are spelled using only the given letters, repeat letters allowed
-    /// 
+    ///
     /// given `letters` a "set" of letters, we only require that each letter of the word is in `letters`
     pub fn find_with_only_letters(&self, letters: &str) -> Vec<&String> {
         let is_valid = move |word: &String| -> bool {
@@ -59,7 +63,7 @@ impl WordList {
     }
 
     /// Find words that could be played given a set of tiles in a scrabble game
-    /// 
+    ///
     /// `tiles` must be a string containing only letters and possibly question marks to represent blank tiles
     pub fn find_scrabble_simple(&self, tiles: &str) -> Vec<&String> {
         let is_valid = move |word: &String| -> bool {
@@ -81,4 +85,31 @@ impl WordList {
         self.words.iter().filter(|word| is_valid(word)).collect()
     }
 
+    pub fn find_starts_with(&self, prefix: &str) -> Vec<&String> {
+        self.words
+            .iter()
+            .filter(|word| word.starts_with(prefix))
+            .collect()
+    }
+
+    pub fn find_ends_with(&self, suffix: &str) -> Vec<&String> {
+        self.words
+            .iter()
+            .filter(|word| word.ends_with(suffix))
+            .collect()
+    }
+
+    pub fn find_contains(&self, substring: &str) -> Vec<&String> {
+        self.words
+            .iter()
+            .filter(|word| word.contains(substring))
+            .collect()
+    }
+
+    pub fn find_length(&self, length: usize) -> Vec<&String> {
+        self.words
+            .iter()
+            .filter(|word| word.len() == length)
+            .collect()
+    }
 }
